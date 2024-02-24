8ae1271c41244f623538492d20399597e76491f2
mrfinker <caalebs@gmail.com>
2024-02-24 18:57:02 +0000

    24 févr. 2024, 19:57

--- script.js
+++ script.js
@@ -2,7 +2,3 @@ let timeNow = new Date();
 
 console.log(timeNow.getHours());
 console.log(timeNow.getMinutes());
-console.log(
-  setInterval(countUp = () => {
-  timeNow.getSeconds() += 1
-}, 1000));
